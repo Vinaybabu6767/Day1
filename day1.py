7349157d4b8b66a001d8d5d1a05f02d787ebd4ac
Vinaybabu6767 <vinayjuttukonda6767@gmail.com>
2021-08-30 21:46:19 +0530

    first commit

---
+++ day1.py
@@ -0,0 +1,7 @@
+user_name=input("what is your name my friend?\n")
+print(f"welcome to Banf-Game-Generator {user_name}")
+city_name =input(f"where did you grew up {user_name}\n")
+pet_name=input(f"what is your pet name {user_name}\n")
+band_name = city_name + pet_name
+print(f"Here is your band name {band_name}")
+
